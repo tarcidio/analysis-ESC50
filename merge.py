3d321d1e2418b92c843c98a40658c85661d284a5
tarcidio <tarcidio.antonio@usp.br>
2022-10-26 02:56:57 -0300

    features

--- merge.py
+++ merge.py
@@ -1,13 +1,14 @@
 import pandas as pd
 import os
 
-CSV_PATH = 'csv_feature'
+CSV_PATH : str = 'csv_feature'
 
 dfs : list = [pd.read_csv(os.path.join(CSV_PATH,i)) for i in os.listdir(CSV_PATH)]
 
 for i in dfs:
    i.set_index(['filename'], drop = True, inplace = True)
 
-result = pd.concat(dfs)
+result : pd.DataFrame = pd.concat(dfs)
 result.reset_index(inplace=True)
-result
+
+result.to_csv(os.path.join(CSV_PATH, 'feature.csv'))
