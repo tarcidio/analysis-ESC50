e188faded6b68312cc4d1366f4aec950e13216d1
tarcidio <tarcidio.antonio@usp.br>
2022-11-09 05:56:49 -0300

    Corrigindo merge.py

--- merge.py
+++ merge.py
@@ -3,12 +3,12 @@ import os
 
 CSV_PATH : str = 'csv_feature'
 
-dfs : list = [pd.read_csv(os.path.join(CSV_PATH,i)) for i in os.listdir(CSV_PATH)]
+dataFrames : list = [pd.read_csv(os.path.join(CSV_PATH,i)) for i in os.listdir(CSV_PATH)]
 
-for i in dfs:
+for i in dataFrames:
    i.set_index(['filename'], drop = True, inplace = True)
 
-result : pd.DataFrame = pd.concat(dfs)
+result : pd.DataFrame = pd.concat(dataFrames)
 result.reset_index(inplace=True)
 
 result.to_csv('feature.csv', index = False)
